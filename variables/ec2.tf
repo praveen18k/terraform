c14d6cf2aebf68ca0640063c5c13de57a58ca613
Praveen <praveenkothapally18@gmail.com>
2024-05-21 22:30:29 +0530

    ec2

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "roboshop"{
     ami = var.ami_id 
     instance_type = var.instance_type
-    security_groups = [aws_security_group.Allow-all-1.name]
-    
+    #security_groups = [aws_security_group.Allow-all-1.name]
+
 }
 
 
