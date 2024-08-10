8d711cb3170ccdc61f442fb07aac03987a66f968
Praveen <praveenkothapally18@gmail.com>
2024-08-11 01:46:49 +0530

    locals

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,3 +1,4 @@
 locals{
     ami_id = "ami-0b4f379183e5706b9"
+    key_public = file("${path.module}/daws74s.pub")
 }
--- locals/test-locals.tf
+++ locals/test-locals.tf
@@ -7,12 +7,22 @@ resource "aws_instance" "conditions" {
   }
 }
 
-resource "aws_route53_record" "record"{
-    count = 5
-    zone_id = var.zone_id
-    name = "${var.instance_names[count.index]}.${var.domain}"
-    type = "A"
-    ttl = 1
-    records = [aws_instance.conditions[count.index].private_ip]
+resource "aws_route53_record" "record" {
+  count   = 5
+  zone_id = var.zone_id
+  name    = "${var.instance_names[count.index]}.${var.domain}"
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.conditions[count.index].private_ip]
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-pub"
+  public_key = local.key_public
+}
+
+resource "aws_instance" "file-function" {
+  ami           = local.ami_id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.deployer.key_name
+}
