c15ad0a1e66e2cdfc1ffbef57b7999e85d832e86
Praveen <praveenkothapally18@gmail.com>
2024-08-10 19:20:25 +0530

    terraform count index

--- count/count.tf
+++ count/count.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "conditions" {
   count         = 5
   ami           = var.ami_id
-  instance_type = var.instance_type
+  instance_type = var.instance_names[count.index] == "MongoDB" || var.instance_names[count.index] == "Redis" ? "t2.medium" : "t2.micro"
   tags = {
     Name = var.instance_names[count.index]
   }
