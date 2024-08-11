e23010c26e0d2c4a5d284a52499e2750bc08f531
Praveen <praveenkothapally18@gmail.com>
2024-08-12 02:30:31 +0530

    updating data source

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -13,3 +13,35 @@ data "aws_ami" "ami_id" {
 output "ami_id" {
   value = data.aws_ami.ami_id.id
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+output "vpc_info" {
+  value = data.aws_vpc.default
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow-port"
+  vpc_id = data.aws_vpc.default.id
+  description = "Allow inbound 80 traffic and all outbound traffic"
+
+  ingress {
+    description = "Allowing all inbound traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# output "aws_security_group" {
+#   value = data.aws_security_group.allow_http.id
+# }
