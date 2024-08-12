45886306cf71e01e3e9d034c8425ac5e96a4f498
Praveen <praveenkothapally18@gmail.com>
2024-08-12 19:35:31 +0530

    terraform ,tfvars for DEV and Prod

--- tfvars/PROD/backend.tf
+++ tfvars/PROD/backend.tf
@@ -1,5 +1,5 @@
-bucket         = "roboshop-remote-state2"
+bucket         = "roboshop-remote-state2-prod"
 key            = "foreach-demo"
 region         = "us-east-1"
-dynamodb_table = "roboshop-locking"
+dynamodb_table = "roboshop-locking-prod"
   
