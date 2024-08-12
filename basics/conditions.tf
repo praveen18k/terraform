874f00edf870e885cca95b62d9210934962d5f20
Praveen <praveenkothapally18@gmail.com>
2024-08-13 01:31:29 +0530

    terraform update

--- basics/conditions.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "conditions" {
-  ami             = var.ami_id
-  instance_type   = var.instance_name == "MongoDB" ? "t2.medium" : "t2.micro"
-}
--- basics/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_name" {
-  default = "MongoDB"
-}
-
-variable "ami_id" {
-  type    = string
-  default = "ami-0b4f379183e5706b9"
-}
-
